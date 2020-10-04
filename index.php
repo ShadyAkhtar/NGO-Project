0a68360a9b5f7a475679e7282f8b1fb863f0b174
Shadab <akhtarshadab415@gmail.com>
2020-10-04 19:52:57 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=7">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="">
+    <title>NGO</title>
+    
+    
+  </head>
+  <body>
+    <?php
+        require 'header.php';
+        ?>
+<!-- Carousel -->
+<div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/child.jpg" class="d-block w-100" alt="Help Poor Family" >
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Be A Reason Of Someone's Smile</h5>
+        <a class="btn btn-primary" href="#" role="button">Donate!</a>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <!-- <img src="img/homeless_old.jpg" class="d-block w-100" alt="Help Homeless"height ="650p"> -->
+      <img src="img/homeless_old_Copy.jpg" class="d-block w-100" alt="Help Homeless">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Help The Homeless</h5>
+        <a class="btn btn-primary" href="#" role="button">Donate!</a>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/poor.jpg" class="d-block w-100" alt="Help Backward And Tribes">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Unite For A Good Cause</h5>
+        <a class="btn btn-primary" href="#" role="button">Donate!</a>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/poor_education.jpg" class="d-block w-100" alt="Educate The Nation" >
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Let Them Learn And Build The New Nation</h5>
+        <a class="btn btn-primary" href="#" role="button">Donate!</a>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!-- Carousel End  -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+</body>
+</html>
